28cdd32653da345f3f5ba52d4bbc05a09b9d898f
Graham Holtslander <graham@holtslander.com>
2015-10-18 14:47:45 -0600

    Better variable names

--- src/app/parsers/dictionary.py
+++ src/app/parsers/dictionary.py
@@ -9,4 +9,4 @@ from bs4 import BeautifulSoup
 def get_definitions(result):
     soup = BeautifulSoup(result, 'lxml')
     # Work some magic.
-    return [text.text.replace('\n', '') for text in soup.find_all(class_='def-content')]
+    return [definition_content.text.replace('\n', '') for definition_content in soup.find_all(class_='def-content')]
