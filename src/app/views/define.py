24104a31d56ec9902f2e6585e043a0b76d024500
Graham Holtslander <graham@holtslander.com>
2015-10-18 08:41:31 -0600

    Split out into BaseResponder and child classes

--- src/app/views/define.py
+++ src/app/views/define.py
@@ -1,53 +1,44 @@
 """
 Definition webhook responder
 """
-import json
 import logging
 import urllib2
 
-from webapp2 import RequestHandler
+from app.views import BaseResponder
 
 from app.parsers.dictionary import get_definitions
 from settings import DEFINE_TOKENS
 
 
-class DefineView(RequestHandler):
+class DefineView(BaseResponder):
     """ Define a word! """
     DICTIONARY_TEMPLATE = 'http://dictionary.reference.com/browse/{}'
 
-    def post(self):
-        token = self.request.POST['token']
-        text = self.request.POST.get('text')
+    @staticmethod
+    def check_credentials(token):
+        return token in DEFINE_TOKENS
 
-        if token in DEFINE_TOKENS:
-            # It's us!
-            term = text.split(':')[1].strip()
+    def process(self, text, trigger_word):
+        term = text.split(':')[1].strip()
 
-            logging.info('Going to town on: %s', term)
+        logging.info('Going to town on: %s', term)
 
-            if ' ' in term:
-                # Won't work so hot at this time, return an error
-                return self.response.write(json.dumps({
-                    'text': 'Only single words are supported right now. :bow:'
-                }))
+        if ' ' in term:
+            # Won't work so hot at this time, return an error
+            return 'Only single words are supported right now. :bow:'
 
-            logging.info('Searching dictionary.com for term: %s', term)
+        logging.info('Searching dictionary.com for term: %s', term)
 
-            try:
-                result = urllib2.urlopen(self.DICTIONARY_TEMPLATE.format(term))
-                definitions = get_definitions(result)
+        try:
+            result = urllib2.urlopen(self.DICTIONARY_TEMPLATE.format(term))
+            definitions = get_definitions(result)
 
-                return self.response.write(json.dumps({
-                    'text': self._format_response(term, definitions)
-                }))
-            except urllib2.URLError as ue:
-                if ue.code == 404:
-                    return self.response.write(json.dumps({
-                        'text': 'Can\'t find anything for *{}*! :bow:'.format(term)
-                    }))
-                return self.response.write(json.dumps({
-                    'text': 'Something went wrong with the request. :bow:'
-                }))
+            return self._format_response(term, definitions)
+        except urllib2.URLError as ue:
+            if ue.code == 404:
+                return 'Can\'t find anything for *{}*! :bow:'.format(term)
+            # We don't know (read: haven't implemented) what went wrong, return something generic.
+            return 'Something went wrong with the request. :bow:'
 
     @staticmethod
     def _format_response(term, definitions):
